b61347d48e41cc3e479ecda0d2eccd7ff5b905f7
SealifeFoam <77304337+SealifeFoam@users.noreply.github.com>
2021-03-29 11:19:04 -0600

    f

---
+++ Homework 12/tutorials/T2.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 400);
+    
+}
+ function draw(){
+     background (50); 
+
+     fill(0,200,255);
+     rect(290,200, 20, 150);
+
+     fill(255, 0, 200);
+     ellipse(300, 200, 100, 100);
+     
+ }
